ec6db5bf744204a8a48804495c989701f495445c
novozhilov1k <novozhilovkirill793@gmail.com>
2026-04-13 13:14:39 +0300

    popitka1

---
+++ BugPro/Program.cs
@@ -0,0 +1,159 @@
+﻿using Stateless;
+
+namespace BugPro;
+
+public class BugInfo
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? AssignedTo { get; set; }
+}
+
+public class Bug
+{
+    public enum State
+    {
+        New,
+        Assigned,
+        InProgress,
+        Fixed,
+        Tested,
+        Closed,
+        Reopened
+    }
+
+    public enum Trigger
+    {
+        Assign,
+        Start,
+        Fix,
+        Test,
+        Close,
+        Reopen
+    }
+
+    private readonly StateMachine<State, Trigger> _machine;
+    private readonly BugInfo _info;
+
+    public Bug(string title, string description)
+    {
+        _info = new BugInfo { Title = title, Description = description };
+        _machine = new StateMachine<State, Trigger>(State.New);
+
+        _machine.Configure(State.New)
+            .Permit(Trigger.Assign, State.Assigned);
+
+        _machine.Configure(State.Assigned)
+            .Permit(Trigger.Start, State.InProgress);
+
+        _machine.Configure(State.InProgress)
+            .Permit(Trigger.Fix, State.Fixed);
+
+        _machine.Configure(State.Fixed)
+            .Permit(Trigger.Test, State.Tested);
+
+        _machine.Configure(State.Tested)
+            .Permit(Trigger.Close, State.Closed);
+
+        _machine.Configure(State.Closed)
+            .Permit(Trigger.Reopen, State.Reopened);
+
+        _machine.Configure(State.Reopened)
+            .Permit(Trigger.Assign, State.Assigned);
+    }
+
+    public State GetState() => _machine.State;
+
+    public void Assign(string assignee)
+    {
+        if (_machine.CanFire(Trigger.Assign))
+        {
+            _info.AssignedTo = assignee;
+            _machine.Fire(Trigger.Assign);
+        }
+    }
+
+    public void Start()
+    {
+        if (_machine.CanFire(Trigger.Start))
+            _machine.Fire(Trigger.Start);
+    }
+
+    public void Fix()
+    {
+        if (_machine.CanFire(Trigger.Fix))
+            _machine.Fire(Trigger.Fix);
+    }
+
+    public void Test()
+    {
+        if (_machine.CanFire(Trigger.Test))
+            _machine.Fire(Trigger.Test);
+    }
+
+    public void Close()
+    {
+        if (_machine.CanFire(Trigger.Close))
+            _machine.Fire(Trigger.Close);
+    }
+
+    public void Reopen()
+    {
+        if (_machine.CanFire(Trigger.Reopen))
+            _machine.Fire(Trigger.Reopen);
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"\nБаг: {_info.Title}");
+        Console.WriteLine($"Описание: {_info.Description}");
+        Console.WriteLine($"Исполнитель: {_info.AssignedTo ?? "не назначен"}");
+        Console.WriteLine($"Состояние: {_machine.State}\n");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== ДЕМОНСТРАЦИЯ РАБОТЫ БАГА ===\n");
+
+        var bug = new Bug("BUG-001", "Ошибка при входе в систему");
+        bug.PrintInfo();
+
+        bug.Assign("Иванов И.И.");
+        bug.PrintInfo();
+
+        bug.Start();
+        bug.PrintInfo();
+
+        bug.Fix();
+        bug.PrintInfo();
+
+        bug.Test();
+        bug.PrintInfo();
+
+        bug.Close();
+        bug.PrintInfo();
+
+        bug.Reopen();
+        bug.PrintInfo();
+
+        bug.Assign("Петров П.П.");
+        bug.PrintInfo();
+
+        bug.Start();
+        bug.PrintInfo();
+
+        bug.Fix();
+        bug.PrintInfo();
+
+        bug.Test();
+        bug.PrintInfo();
+
+        bug.Close();
+        bug.PrintInfo();
+
+        Console.WriteLine("=== ДЕМОНСТРАЦИЯ ЗАВЕРШЕНА ===");
+    }
+}
---
+++ BugTests/Test1.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BugPro;
+
+namespace BugTests;
+
+[TestClass]
+public class BugTests
+{
+    [TestMethod]
+    public void InitialState_IsNew()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        Assert.AreEqual(Bug.State.New, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Assign_FromNew_StateBecomesAssigned()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Assign("Ivanov");
+        Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Start_FromAssigned_StateBecomesInProgress()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Assign("Ivanov");
+        bug.Start();
+        Assert.AreEqual(Bug.State.InProgress, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Fix_FromInProgress_StateBecomesFixed()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Assign("Ivanov");
+        bug.Start();
+        bug.Fix();
+        Assert.AreEqual(Bug.State.Fixed, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Test_FromFixed_StateBecomesTested()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Assign("Ivanov");
+        bug.Start();
+        bug.Fix();
+        bug.Test();
+        Assert.AreEqual(Bug.State.Tested, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Close_FromTested_StateBecomesClosed()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Assign("Ivanov");
+        bug.Start();
+        bug.Fix();
+        bug.Test();
+        bug.Close();
+        Assert.AreEqual(Bug.State.Closed, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Reopen_FromClosed_StateBecomesReopened()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Assign("Ivanov");
+        bug.Start();
+        bug.Fix();
+        bug.Test();
+        bug.Close();
+        bug.Reopen();
+        Assert.AreEqual(Bug.State.Reopened, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Assign_FromReopened_StateBecomesAssigned()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Assign("Ivanov");
+        bug.Start();
+        bug.Fix();
+        bug.Test();
+        bug.Close();
+        bug.Reopen();
+        bug.Assign("Petrov");
+        Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Start_FromNew_StateRemainsNew()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Start();
+        Assert.AreEqual(Bug.State.New, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Fix_FromAssigned_StateRemainsAssigned()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Assign("Ivanov");
+        bug.Fix();
+        Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Close_FromInProgress_StateRemainsInProgress()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Assign("Ivanov");
+        bug.Start();
+        bug.Close();
+        Assert.AreEqual(Bug.State.InProgress, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Reopen_FromTested_StateRemainsTested()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Assign("Ivanov");
+        bug.Start();
+        bug.Fix();
+        bug.Test();
+        bug.Reopen();
+        Assert.AreEqual(Bug.State.Tested, bug.GetState());
+    }
+
+    [TestMethod]
+    public void FullWorkflow_CompletesSuccessfully()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        
+        Assert.AreEqual(Bug.State.New, bug.GetState());
+        bug.Assign("Ivanov");
+        Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+        bug.Start();
+        Assert.AreEqual(Bug.State.InProgress, bug.GetState());
+        bug.Fix();
+        Assert.AreEqual(Bug.State.Fixed, bug.GetState());
+        bug.Test();
+        Assert.AreEqual(Bug.State.Tested, bug.GetState());
+        bug.Close();
+        Assert.AreEqual(Bug.State.Closed, bug.GetState());
+        bug.Reopen();
+        Assert.AreEqual(Bug.State.Reopened, bug.GetState());
+        bug.Assign("Petrov");
+        Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+    }
+
+    [TestMethod]
+    public void Assign_MultipleTimes_StateRemainsAssigned()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        bug.Assign("Ivanov");
+        bug.Assign("Petrov");
+        Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+    }
+
+    [TestMethod]
+    public void GetState_ReturnsCurrentState()
+    {
+        var bug = new Bug("BUG-001", "Test");
+        Assert.AreEqual(Bug.State.New, bug.GetState());
+    }
+}
